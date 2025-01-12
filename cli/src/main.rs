72e0f1669fe9353653f0fd5db835bf188b657041
noreplydev <contact.cristiansanchez@gmail.com>
2025-01-12 16:44:01 +0100

    list serialport names

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,39 @@
+use serialport::{SerialPortInfo, SerialPortType};
+
 mod serial;
 // use espflash::{self, flasher::Flasher, targets::Chip};
 
 fn main() {
-    println!("Hello, cli!");
+    println!("self-esp toolkit");
+
+    println!(">>>> select port <<<<");
     let ports = serial::detect_usb_serial_ports(true);
-    println!("{:#?}", ports);
+    let mut port_names = vec![];
+    match ports {
+        Ok(ports) => {
+            port_names = ports
+                .iter()
+                .map(|port_info| {
+                    let name = port_info.port_name.as_str();
+                    match &port_info.port_type {
+                        SerialPortType::UsbPort(info) => {
+                            if let Some(product) = &info.product {
+                                format!("{} - {}", name, product)
+                            } else {
+                                name.to_string()
+                            }
+                        }
+                        _ => name.to_string(),
+                    }
+                })
+                .collect::<Vec<_>>();
+        }
+        Err(_) => {
+            panic!("Error getting serial ports")
+        }
+    }
+
+    println!("{:#?}", port_names);
     //serial::get_serial_port_info(, config);
     //espflash::connection::Connection::new(, port_info);
 }
