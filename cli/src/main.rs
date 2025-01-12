ac77b7f5ed72ec817a0ca02a6ec40222984c3805
noreplydev <contact.cristiansanchez@gmail.com>
2025-01-12 17:09:39 +0100

    Add serialport openning

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,9 @@
+use std::{
+    io::{self, Write},
+    time::Duration,
+    usize,
+};
+
 use serialport::{SerialPortInfo, SerialPortType};
 
 mod serial;
@@ -6,34 +12,57 @@ mod serial;
 fn main() {
     println!("self-esp toolkit");
 
-    println!(">>>> select port <<<<");
+    println!("\n>>>> select port <<<<\n");
     let ports = serial::detect_usb_serial_ports(true);
     let mut port_names = vec![];
-    match ports {
+    let ports = match ports {
         Ok(ports) => {
             port_names = ports
                 .iter()
-                .map(|port_info| {
-                    let name = port_info.port_name.as_str();
-                    match &port_info.port_type {
-                        SerialPortType::UsbPort(info) => {
-                            if let Some(product) = &info.product {
-                                format!("{} - {}", name, product)
-                            } else {
-                                name.to_string()
-                            }
-                        }
-                        _ => name.to_string(),
-                    }
-                })
+                .map(|port_info| port_info.port_name.clone())
                 .collect::<Vec<_>>();
+            ports
         }
         Err(_) => {
             panic!("Error getting serial ports")
         }
+    };
+
+    port_names
+        .iter()
+        .enumerate()
+        .for_each(|(i, port)| println!("[{i}] {port}"));
+
+    print!("> ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let input = input.trim();
+    let port_name = port_names[input
+        .parse::<usize>()
+        .expect("Input must be a number within the options range")]
+    .clone();
+    let baud_rate = 115200;
+    let port;
+
+    match serialport::new(port_name.clone(), baud_rate)
+        .timeout(Duration::from_millis(100)) // Set a timeout
+        .open() // Open the port
+    {
+        Ok(_port) => {
+            println!("Serial port {} opened successfully!", port_name);
+            port = _port;
+        }
+        Err(e) => {
+            eprintln!("Failed to open serial port: {}", e);
+        }
     }
 
-    println!("{:#?}", port_names);
     //serial::get_serial_port_info(, config);
-    //espflash::connection::Connection::new(, port_info);
+
+    //espflash::connection::Connection::new(, );
 }
